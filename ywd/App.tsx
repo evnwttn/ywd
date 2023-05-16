967ea9c7b7c42c099e7f7fee0e927194e1a904b9
evnwttn <evnwttn@gmail.com>
2023-05-16 16:11:00 -0230

    splash

--- ywd/App.tsx
+++ ywd/App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Pressable, Text } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Saira-BlackItalic": require("./assets/fonts/Saira-BlackItalic.ttf"),
+  });
+
+  const onPress = () => {
+    console.log("yo!");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Pressable style={styles.button} onPress={onPress}>
+        <Text style={styles.text}>ywd</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundImage: "linear-gradient(#DF8B60, #BC859C)",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {},
+  text: {
+    fontSize: 44,
+    fontFamily: "Saira-BlackItalic",
+    textTransform: "uppercase",
+    color: "#3E2952",
   },
 });
